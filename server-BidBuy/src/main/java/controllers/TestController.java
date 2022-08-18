5b7692907d90a214cba50b39cdd36dcc1ecbee47
ashrhmn <ashrhmn99@gmail.com>
2022-08-19 05:43:50 +0600

    Add firebase upload feature

--- server-BidBuy/src/main/java/controllers/TestController.java
+++ server-BidBuy/src/main/java/controllers/TestController.java
@@ -3,40 +3,48 @@ package controllers;
 
 import dtos.AuthPayloadDto;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import services.FirebaseService;
 
-import javax.crypto.SecretKey;
 import java.io.UnsupportedEncodingException;
-import java.security.Key;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/test")
 public class TestController {
 
+    private final FirebaseService firebaseService;
     private final String keyStr = "nb2ZIMfwdDYi6MNMAGJHFvnNh7jfX+BQWKo8Krl1xB8=";
 
     @GetMapping("/")
     public String create() throws UnsupportedEncodingException {
         return Jwts.
                 builder()
-                .setSubject(AuthPayloadDto.serialize(1,"ash","ash-ash","admin"))
+                .setSubject(AuthPayloadDto.serialize(1, "ash", "ash-ash", "admin"))
                 .signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(keyStr)))
                 .compact();
     }
 
     @GetMapping("/test")
-    public AuthPayloadDto test(@RequestHeader("token") String token){
-        if(token==null) return null;
+    public AuthPayloadDto test(@RequestHeader("token") String token) {
+        if (token == null) return null;
         String payload = Jwts.parserBuilder()
                 .setSigningKey(Decoders.BASE64.decode(keyStr)).build().parseClaimsJws(token).getBody().getSubject();
         return AuthPayloadDto.deserialize(payload);
+    }
+
+    @PostMapping("/firebase")
+    public String firebase(@RequestParam("file") MultipartFile multipartFile) {
+        return firebaseService.upload(multipartFile);
+    }
+
+    public String test2() {
+        String name = "dfsbafb";
+        return name.substring(name.lastIndexOf("."));
     }
 
 }
