4619807065cfa42547ab9f7e5bf6df8b923c30f0
ashrhmn <ashrhmn99@gmail.com>
2022-08-21 21:48:40 +0600

    Add bid

--- server-BidBuy/src/main/java/services/ProductService.java
+++ server-BidBuy/src/main/java/services/ProductService.java
@@ -1,19 +1,26 @@
 package services;
 
-import dtos.ProductDto;
 import model.Product;
 
 import java.util.List;
 
 public interface ProductService {
-    public List<ProductDto> getAll(int page, int viewPerPage);
-    public List<ProductDto> getProductBySellerId(int page, int viewPerPage, int sellerId);
+    public List<Product> getAll(int page, int viewPerPage);
+
+    public List<Product> getProductBySellerId(int page, int viewPerPage, int sellerId);
+
     public Integer getAllCount();
-    public ProductDto getById(int id);
+
+    public Product getById(int id);
+
     public Product getByName(String name);
+
     public void save(Product product);
+
     public void update(Product product);
+
     public void delete(int id);
+
     public void changeStatus(int id, String status);
 
 }
