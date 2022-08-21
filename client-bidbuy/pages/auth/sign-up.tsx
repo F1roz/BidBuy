bbfa1eeeea33c682e86d138c66c52f30c430fffb
hasanjahidul <jahidul0hasan@gmail.com>
2022-08-21 23:03:55 +0600

    sign up form update

--- client-bidbuy/pages/auth/sign-up.tsx
+++ client-bidbuy/pages/auth/sign-up.tsx
@@ -56,6 +56,22 @@ const SignUp = () => {
                 </div>
                 <div>
                   <label
+                    htmlFor="nid"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your Nid Number
+                  </label>
+                  <input
+                    type="nid"
+                    name="nid"
+                    id="nid"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder=""
+                    required
+                  />
+                </div>
+                <div>
+                  <label
                     htmlFor="password"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
